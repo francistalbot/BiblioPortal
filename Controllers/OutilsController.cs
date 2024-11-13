e5aba6a1c396773a107202ddcc3107c452334fec
franc <ftalbot@anekdote.ca>
2024-11-13 15:53:06 -0500

    Creation of first Controler, Model and View; Outil.

---
+++ Controllers/OutilsController.cs
@@ -0,0 +1,16 @@
+﻿using BiblioPortal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiblioPortal.Controllers
+{
+    public class OutilsController : Controller
+    {
+        //GET: Outils/Random
+        public IActionResult Random()
+        {
+            var outil = new Outil() { Name = "Marteau" };
+
+            return View(outil);
+        }
+    }
+}
---
+++ Models/Outil.cs
@@ -0,0 +1,9 @@
+﻿namespace BiblioPortal.Models
+{
+    public class Outil
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    
+}
