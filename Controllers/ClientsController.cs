fc425d46d7f4a1a7d6d387540c4b99a7a5808260
franc <ftalbot@anekdote.ca>
2024-11-15 11:04:27 -0500

    Create Client Controller and Index view

---
+++ Controllers/ClientsController.cs
@@ -0,0 +1,19 @@
+﻿using BiblioPortal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiblioPortal.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private BiblioDbContext _context;
+        public ClientsController(BiblioDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var clients = _context.Clients;
+            return View(clients);
+        }
+    }
+}
