1dae66cb4f3ff4d2195e898234f68a73e305eba5
franc <ftalbot@anekdote.ca>
2024-11-18 13:25:05 -0500

    Add Details view

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -21,5 +21,14 @@ namespace BiblioPortal.Controllers
             var clients = _context.Clients.Include(c => c.MembershipType);
             return View(clients);
         }
+        public ViewResult Details(int id)
+        {
+            var client = _context.Clients
+                .Include(c => c.MembershipType)     //Inclut les MembershipType en Relation
+                .Include(c => c.Locations)          //Inclut les Locations en Relation
+                .ThenInclude(c => c.Outil)          //Ensuit inclut les outils en relation avec les Locations
+                .SingleOrDefault(c => c.Id == id);
+            return View(client);
+        }
     }
 }
