6c95e21ef68cb7e889b2018ff03d7f2f2575a9e1
franc <ftalbot@anekdote.ca>
2024-11-18 14:44:07 -0500

    Modification of client details

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -1,5 +1,7 @@
 ﻿using BiblioPortal.Models;
+using BiblioPortal.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BiblioPortal.Controllers
 {
@@ -32,20 +34,54 @@ namespace BiblioPortal.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Client client) 
+        public IActionResult Save(Client client) 
         { 
-            _context.Clients.Add(client);
+            if (client.Id == 0) 
+                _context.Clients.Add(client);
+            else
+            {
+                var clientInDb = _context.Clients.Single(c => c.Id == client.Id);
+                clientInDb.Name = client.Name;
+                clientInDb.DateOfBirth = client.DateOfBirth;
+                clientInDb.Phone = client.Phone;
+                clientInDb.Email = client.Email;
+                clientInDb.IsSubscribeToNewsletter
+                    = client.IsSubscribeToNewsletter;
+
+
+            }
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Clients"); 
         }    
+        public IActionResult Details(int id)
         {
             var client = _context.Clients
                 .Include(c => c.MembershipType)     //Inclut les MembershipType en Relation
                 .Include(c => c.Locations)          //Inclut les Locations en Relation
                 .ThenInclude(c => c.Outil)          //Ensuit inclut les outils en relation avec les Locations
                 .SingleOrDefault(c => c.Id == id);
+
+            if (client == null)
+                return NotFound(); // Renvoie une erreur 404 si l'outil n'existe pas
             return View(client);
+        }
+        public IActionResult Edit(int id)
+        {
+            var client = _context.Clients
+                .Include(c => c.MembershipType)     //Inclut les MembershipType en Relation
+                .Include(c => c.Locations)          //Inclut les Locations en Relation
+                .ThenInclude(c => c.Outil)          //Ensuit inclut les outils en relation avec les Locations
+                .SingleOrDefault(c => c.Id == id);
+
+            if (client == null)
+                return NotFound(); // Renvoie une erreur 404 si l'outil n'existe pas
+            var viewmodel = new ClientFormViewModel
+            {
+                Client = client,
+                MembershipTypes = _context.MembershipTypes,
+            };
+            return View("ClientForm", viewmodel);
         }
     }
 }
