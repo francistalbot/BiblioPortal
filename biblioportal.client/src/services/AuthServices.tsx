cef38b56079e2adfc44800f1dfd6917e20ecf51a
Francis-Laptop <francis.talbot@hotmail.com>
2025-02-25 08:13:20 -0500

    Add authentification API services to the authservices
    Add register, refresh, confirmEmail, resenConfirmationEmail, forgotPassword, resetPassword, manageInfo and Manage2fa

--- biblioportal.client/src/services/AuthServices.tsx
+++ biblioportal.client/src/services/AuthServices.tsx
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const API_URL = "https://localhost:7168"; // Replace with your backend URL
+const API_URL = "http://localhost:5147"; 
 
 const axiosInstance = axios.create({
     withCredentials: true,
@@ -10,17 +10,106 @@ const axiosInstance = axios.create({
     headers: { "Content-Type": "application/json" },
 });
 
-interface UserData {
+//POST Register user
+interface RegisterData {
+    email: string;
+    password: string;
+}
+const register = async (registerData: RegisterData) => {
+    const response = await axiosInstance.post("/register", registerData);
+    return response.data;
+};
+
+//POST Login user
+interface LoginData {
     email: string;
     password: string;
     twoFactorCode: string;
     twoFactorRecoveryCode: string;
-
 }
-// Login user
-const login = async (userData: UserData ) => {
-     const response = await axiosInstance.post("/login", userData);
+const login = async (loginData: LoginData ) => {
+    const response = await axiosInstance.post("/login", loginData);
     return response.data;
 };
 
-export { login };
+//POST refresh
+interface RefreshData {
+    refreshToken: string;
+}
+const refresh = async (refreshData: RefreshData) => {
+    const response = await axiosInstance.post("/refresh", refreshData);
+    return response.data;
+};
+
+//GET confirmEmail
+interface ConfirmEmailParams {
+    userId: string;
+    code: string;
+    changedEmail: string;
+}
+const confirmEmail = async (confirmEmailParams: ConfirmEmailParams) => {
+    const response = await axiosInstance.get("/confirmEmail", { params: confirmEmailParams });
+    return response.data;
+};
+
+//POST resend a Confirmation Email
+interface ResendConfEmailData {
+    email: string;
+}
+const resendConfirmationEmail = async (resendConfEmailData: ResendConfEmailData) => {
+    const response = await axiosInstance.post("/resendConfirmationEmail", resendConfEmailData);
+    return response.data;
+};
+
+//POST forgot Password
+interface ForgotPassData {
+    email: string;
+}
+const forgotPassword = async (forgotPassData: ForgotPassData) => {
+    const response = await axiosInstance.post("/forgotPassword", forgotPassData);
+    return response.data;
+};
+
+//POST Reset Password
+interface ResetPasswordData {
+    email: string;
+    resetCode: string,
+    newPassword: string
+}
+const resetPassword = async (resetPasswordData: ResetPasswordData) => {
+    const response = await axiosInstance.post("/resetPassword", resetPasswordData);
+    return response.data;
+};
+
+//GET manage info
+const getManageInfo = async () => {
+    const response = await axiosInstance.get("/manage/info");
+    return response.data;
+};
+//POST manage 2fa
+interface Manage2faData {
+    enable: boolean,
+    twoFactorCode: string,
+    resetSharedKey: boolean,
+    resetRecoveryCodes: boolean,
+    forgetMachine: boolean
+}
+const manage2fa = async (manage2faData: Manage2faData) => {
+    const response = await axiosInstance.post("/manage/2fa", manage2faData);
+    return response.data;
+};
+//POST manage info
+interface PostManageInfoData {
+    newEmail: string,
+    newPassword: string,
+    oldPassword: string
+}
+const postManageInfo = async (postManageInfoData: PostManageInfoData) => {
+    const response = await axiosInstance.post("/manage/info", postManageInfoData);
+    return response.data;
+};
+export {
+    register, login, refresh, confirmEmail,
+    resendConfirmationEmail, forgotPassword, resetPassword,
+    manage2fa, getManageInfo, postManageInfo,
+};
